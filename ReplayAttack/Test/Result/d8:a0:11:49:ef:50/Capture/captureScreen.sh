baebd0b67292f204dfca7ef1814f8a3a34a501d8
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-08-03 09:57:11 +0200

    v

--- ReplayAttack/Test/Result/d8:a0:11:49:ef:50/Capture/captureScreen.sh
+++ ReplayAttack/Test/Result/d8:a0:11:49:ef:50/Capture/captureScreen.sh
@@ -6,6 +6,7 @@ function waitphone {
     done
 }
 
+name="$1"
 ANDROID_SERIAL="954AY0U0EE"
 echo Phone is: $PHONE/$ANDROID_SERIAL
 PHONE_FOUND=`adb devices | grep $ANDROID_SERIAL | grep device`
@@ -14,8 +15,8 @@ echo Phone ready, proceeding...
 
 
 waitphone
-adb -s $ANDROID_SERIAL shell -n screencap -p /sdcard/reference.png
+adb -s $ANDROID_SERIAL shell -n screencap -p /sdcard/screen_exp.png
 waitphone
-adb -s $ANDROID_SERIAL pull /sdcard/reference.png ./reference.png
+adb -s $ANDROID_SERIAL pull /sdcard/screen_exp.png ./${name}_reference.png
 waitphone
 adb -s $ANDROID_SERIAL shell -n rm /sdcard/screen_exp.png
