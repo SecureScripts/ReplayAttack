1310598a86588a43abb9a64fa931287e48176695
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-08-04 09:10:32 +0200

    Update

--- ReplayAttack/Test/captureScreenshoot.sh
+++ ReplayAttack/Test/captureScreenshoot.sh
@@ -3,12 +3,9 @@
 CAPT_DIR="$1"
 ANDROID_SERIAL="$2"
 
-waitphone
+
 adb -s $ANDROID_SERIAL shell input keyevent 224 
 sleep 2s
-waitphone
 adb -s $ANDROID_SERIAL shell -n screencap -p /sdcard/screen_exp.png
-waitphone
 adb -s $ANDROID_SERIAL pull /sdcard/screen_exp.png $CAPT_DIR/screen_exp.png
-waitphone
 adb -s $ANDROID_SERIAL shell -n rm /sdcard/screen_exp.png
