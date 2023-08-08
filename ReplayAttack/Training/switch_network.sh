c55796d2109a8af46ee52d59e122b405cd4f880a
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-08-08 10:53:23 +0200

    Update

--- ReplayAttack/Training/switch_network.sh
+++ ReplayAttack/Training/switch_network.sh
@@ -7,8 +7,9 @@ PASSWORD=$(cat local_networks.txt | grep $INTERFACE | awk '{ print $3 }')
 curr_SSID=$(adb -s $ANDROID_SERIAL shell dumpsys wifi | grep "current SSID")
 echo "$curr_SSID"
 #echo 'current SSID(s):{iface=wlan0,ssid="iotlab4"}' | cut -d'=' -f 3 | cut -d'"' -f 2
-adb -s $ANDROID_SERIAL shell am start -n com.steinwurf.adbjoinwifi/.MainActivity -e ssid $SSID -e password_type WPA -e password $PASSWORD #da verificare che funziona quando la rete è già nota!
-#adb shell am start -n com.steinwurf.adbjoinwifi/.MainActivity -e ssid SSID
+#adb -s $ANDROID_SERIAL shell am start -n com.steinwurf.adbjoinwifi/.MainActivity -e ssid $SSID -e password_type WPA -e password $PASSWORD #da verificare che funziona quando la rete è già nota!
+adb -s $ANDROID_SERIAL shell am start -n com.steinwurf.adbjoinwifi/.MainActivity -e ssid SSID
+sleep 5s
 adb -s $ANDROID_SERIAL shell am force-stop com.steinwurf.adbjoinwifi
 #per il check della rete corrente dopo il cambio rete
 new_SSID=$(adb -s $ANDROID_SERIAL shell dumpsys wifi | grep "current SSID")
