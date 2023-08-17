34c340821b9c52285ee4dd5e434cb1444b9c850f
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-08-17 13:21:20 +0200

    Update

--- ReplayAttack/Training/trigger_functionality_training.sh
+++ ReplayAttack/Training/trigger_functionality_training.sh
@@ -43,12 +43,16 @@ do
 temp=$(../Test/trigger_functionality_testing.sh "$ANDROID_SERIAL" "$PACKAGE" "../Test/Result/$MAC_DEVICE/Capture" $CROP_FUN "../Test/Result/$MAC_DEVICE/Fun_coordinates.txt" Fun True $tap_time $open_time)
  if [[ "${temp##*$'\n'}" != "Comparison ok" ]]
         then
-        echo "Something goes wrong"
+        echo "Something goes wrong: Fun"
+ else
+  echo "Fun OK"
  fi
 sleep 2s
 temp=$(../Test/trigger_functionality_testing.sh "$ANDROID_SERIAL" "$PACKAGE" "../Test/Result/$MAC_DEVICE/Capture" $CROP_REVERSE "../Test/Result/$MAC_DEVICE/Reverse_coordinates.txt" Reverse True $tap_time $open_time)
  if [[ "${temp##*$'\n'}" != "Comparison ok" ]]
         then
-        echo "Something goes wrong"
+        echo "Something goes wrong: Reverse"
+ else
+  echo "Reverse OK"
  fi
 done
