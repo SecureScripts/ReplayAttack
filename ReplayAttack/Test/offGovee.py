0adeb97120dafa0f15321701feb78f332ca6cc33
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-09-11 10:03:08 +0200

    Update offGovee.py

--- ReplayAttack/Test/offGovee.py
+++ ReplayAttack/Test/offGovee.py
@@ -17,19 +17,27 @@ ttl = struct.pack('b', 1)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
 
-UDP_IP = "10.10.0.1"
-UDP_PORT = 4002
-sockClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-sockClient.bind((UDP_IP, UDP_PORT))
+MCAST_GRP = "239.255.255.250"
+MCAST_PORT = 4002
+
+sockClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sockClient.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sockClient.bind(('', MCAST_PORT))
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
+sockClient.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+
 
 try:
     # Send data to the multicast group
     print('sending "%s"' % message)
     sent = sock.sendto(message.encode("UTF-8"), multicast_group)
     while True:
-        data, addr = sockClient.recvfrom(1024)  # buffer size is 1024 bytes
-        print("received message: %s" % data)
-
+        print("Listening on {}".format(MCAST_PORT))
+        if sockClient.recv:
+            print(sockClient.recv(10240))
+            break
 finally:
     print('closing socket')
     sock.close()
