3ef58b5dbe775a51710805f506813d77cffefab3
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-09-11 15:54:44 +0200

    Update switch_network.sh

--- ReplayAttack/Test/switch_network.sh
+++ ReplayAttack/Test/switch_network.sh
@@ -31,6 +31,8 @@ echo "after enabling the ip address is $check"
 #per il check della rete corrente
 curr_SSID=$(adb -s $ANDROID_SERIAL shell dumpsys wifi | grep "current SSID")
 echo $curr_SSID
+if [[ $curr_SSID != *$SSID* ]]
+then
 #echo 'current SSID(s):{iface=wlan0,ssid="iotlab4"}' | cut -d'=' -f 3 | cut -d'"' -f 2
 adb -s $ANDROID_SERIAL shell am start -n com.steinwurf.adbjoinwifi/.MainActivity -e ssid $SSID -e password_type WPA -e password $PASSWORD #da verificare ch>#adb -s $ANDROID_SERIAL shell am start -n com.steinwurf.adbjoinwifi/.MainActivity -e ssid $SSID
 sleep 5s
@@ -46,3 +48,4 @@ echo $check
 #per trovare il mac
 mac_smartphone=$(adb -s $ANDROID_SERIAL shell ip address show wlan0 | grep  link/ether | awk '{ print $2 }')
 echo $mac_smartphone
+fi
