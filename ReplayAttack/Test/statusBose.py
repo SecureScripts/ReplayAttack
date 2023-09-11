991fd1f3b29b9895e11a272b80d80e5ca95c9c66
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-09-11 18:19:00 +0200

    Update statusBose.py

--- ReplayAttack/Test/statusBose.py
+++ ReplayAttack/Test/statusBose.py
@@ -3,11 +3,22 @@ import sys
 from libsoundtouch import soundtouch_device
 from libsoundtouch.utils import Source, Type
 
+from libsoundtouch import discover_devices
+
+devices = discover_devices(timeout=2)  # Default timeout is 5 seconds
+
+
+for device in devices:
+    print(device.config.name + " - " + device.config.type)
+
 
 
 stat=str(sys.argv[1])
 
 device = soundtouch_device('10.12.0.27')
+
+
+
 
 if(stat=="on"):
   device.power_on()
