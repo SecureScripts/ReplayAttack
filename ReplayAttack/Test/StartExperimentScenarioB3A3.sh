d1cc62f0e445e4c50a1cc8a467b7b2c73f53f03f
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-09-15 19:57:55 +0200

    Update StartExperimentScenarioB3A3.sh

--- ReplayAttack/Test/StartExperimentScenarioB3A3.sh
+++ ReplayAttack/Test/StartExperimentScenarioB3A3.sh
@@ -13,17 +13,18 @@
 #WLAN 0
 #YEELIGHT-GOOVE-LEPRO-SONOS SPEAKER
 
+chmod 777 *
 
-#./Govee_Test_device_Restart.sh 93PAY0E1MW d4:ad:fc:60:43:9a wlan0 com.govee.home 28:e3:47:8e:cf:a8 tapo_plug110_25
+./Govee_Test_device_Restart.sh 93PAY0E1MW d4:ad:fc:60:43:9a wlan0 com.govee.home 28:e3:47:8e:cf:a8 tapo_plug110_25
 
 ./Sonos_Test_device_Restart.sh 93PAY0E1MW 38:42:0b:68:76:30 wlan0 com.sonos.acr2 28:e3:47:8e:cf:a8 tapo_plug110_33
 
-#./Test_device_Restart.sh 93PAY0E1MW cc:8c:bf:cb:e1:df wlan0 com.lampux.ledbrighter 92:a3:f9:fe:6c:e9 tapo_plug110_25
+./Test_device_Restart.sh 93PAY0E1MW cc:8c:bf:cb:e1:df wlan0 com.lampux.ledbrighter 92:a3:f9:fe:6c:e9 tapo_plug110_25
 
-#./Test_device_Restart.sh 93PAY0E1MW ec:4d:3e:16:9b:e9 wlan0 com.yeelight.cherry 92:a3:f9:fe:6c:e9 tapo_plug110_4
+./Test_device_Restart.sh 93PAY0E1MW ec:4d:3e:16:9b:e9 wlan0 com.yeelight.cherry 92:a3:f9:fe:6c:e9 tapo_plug110_4
 
 
-./Test_device_Restart.sh 93PAY0E1MW fc:67:1f:a9:a7:8d wlan0 com.tuya.smartlife 92:a3:f9:fe:6c:e9 tapo_plug110_12
+./Test_device_Restart.sh 93PAY0E1MW fc:67:1f:a9:a7:8d wlan0 com.tuya.smartlife 92:a3:f9:fe:6c:e9 tapo_plug110_25
 #WLAN 3
 #EUFI MEROS
 
