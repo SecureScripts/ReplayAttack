473001cca1d95f842b3cd9d83032d8bd9d939d66
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-09-15 16:06:04 +0200

    Update

--- ReplayAttack/Test/FastScenarioB/Fast_Test_device_Restart.sh
+++ ReplayAttack/Test/FastScenarioB/Fast_Test_device_Restart.sh
@@ -33,6 +33,9 @@ filter="(ether src  $MAC_DEVICE and ether dst $MAC_SMARTPHONE) or (ether dst $MA
 START=1
 END=1
 
+
+./CaptureFunctionality.sh $ANDROID_SERIAL $MAC_DEVICE $INTERFACE $PACKAGE $MAC_SMARTPHONE $PLUG_NAME
+
 for (( i=$START; i<=$END; i++ ))
 do
    result="Experiment Failed"
@@ -65,46 +68,11 @@ do
         fi
     fi
    
-   echo "#############################STARTING SNIFFING#####################################"
-    tshark -i "$INTERFACE" -f "$filter" -w "$EXP_FOLDER/capture.pcap" -a duration:"$sniffing_time" &
-    sleep 10s
-
-    temp=$(./trigger_functionality_testing.sh $ANDROID_SERIAL $PACKAGE Result/$MAC_DEVICE/Capture $CROP_FUN Result/$MAC_DEVICE/Fun_coordinates.txt Fun True $tap_time $open_time)
-    wait
-    chmod +r "$EXP_FOLDER"/capture.pcap
-        if [[ "${temp##*$'\n'}" != "Comparison ok" ]]
-        then
-        echo "Experiment Failed"
-        result="Experiment Failed: Repeat"
-        continue
-        fi
-
-    sleep 5s
-
-   ./Restart_device.sh $PLUG_NAME
-
-    sleep 30s
-
-    echo "CHECK THE INITIAL STATE"
-    temp=$(./trigger_functionality_testing.sh $ANDROID_SERIAL $PACKAGE Result/$MAC_DEVICE/Capture $CROP_REVERSE Result/$MAC_DEVICE/Ground_coordinates.txt Reverse True $tap_time $open_time)
-    echo "THE INITIAL STATE IS ${temp##*$'\n'}"
-    if [[ "${temp##*$'\n'}" != "Comparison ok" ]]
-    then
-    	echo "REVERSING THE STATE"
-    	 temp=$(./trigger_functionality_testing.sh $ANDROID_SERIAL $PACKAGE Result/$MAC_DEVICE/Capture $CROP_REVERSE Result/$MAC_DEVICE/Reverse_coordinates.txt Reverse True $tap_time $open_time)
-        if [[ "${temp##*$'\n'}" != "Comparison ok" ]]
-        then
-        echo "Experiment Failed"
-        result="Experiment Failed: Repeat"
-        continue
-        fi
-    fi
-
 
 
 
    echo "#############################STARTING ATTACK AFTER DELAY #####################################"
-   python3 ReplayAttack.py $EXP_FOLDER/capture.pcap $MAC_SMARTPHONE $MAC_DEVICE $delay_time $MODEL_FOLDER > $EXP_FOLDER/res.txt
+   python3 ReplayAttack.py Result/$MAC_DEVICE/Experiments/Delayed/capture.pcap $MAC_SMARTPHONE $MAC_DEVICE $delay_time $MODEL_FOLDER > $EXP_FOLDER/res.txt
 
 
   echo "CHECK GROUND TRUTH"
