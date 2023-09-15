e8dc1878054189c2ac95dff8b6d55670476f0c5e
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-09-15 12:52:30 +0200

    Update

--- ReplayAttack/Test/Restart_device.sh
+++ ReplayAttack/Test/Restart_device.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NAME_PLUG="$1"
+NAME_PLUG=$1
 
-python3 kasa-off.py --plugid $NAME_PLUG --username "iotlabUCL@gmail.com" --password "IoTlabUCL"
+python3 kasa-off.py --plugid $NAME_PLUG --username iotlabUCL@gmail.com --password IoTlabUCL
 sleep 5s
-python3 kasa-on.py --plugid $NAME_PLUG --username "iotlabUCL@gmail.com" --password "IoTlabUCL"
+python3 kasa-on.py --plugid $NAME_PLUG --username iotlabUCL@gmail.com --password IoTlabUCL
