41d97cfc1e5acc415b449fd868074d095228349d
SecureScripts <141262915+SecureScripts@users.noreply.github.com>
2023-09-19 19:58:50 +0200

    Update

---
+++ ReplayAttack/Training/NanoLeaf_Training_device.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+ANDROID_SERIAL="$1"
+MAC_DEVICE="$2"
+INTERFACE="$3"
+PACKAGE="$4"
+
+
+training_times=5
+tap_time="5s"
+open_time="10s"
+
+
+tshark_time=$((40))
+tshark_time=$((tshark_time*2))
+echo $tshark_time
+#temp=$(./switch_network.sh $ANDROID_SERIAL $INTERFACE)
+#MAC_SMARTPHONE="${temp##*$'\n'}"
+MAC_SMARTPHONE=$5
+#echo $MAC_SMARTPHONE
+
+#temp_target_ip_address=$(arp -a | grep $MAC_DEVICE | awk '{print $2}')
+#let len=${#temp_target_ip_address}-1
+#IP_DEVICE=$(cut -c 2-$len <<< $temp_target_ip_address)
+#iptables -I FORWARD 1 -i $INTERFACE -o eth1 -m mac --mac-source $MAC_DEVICE -j DROP
+#iptables -I FORWARD 2 -i eth1 -o $INTERFACE -j DROP -d $IP_DEVICE
+#trap 'iptables -D FORWARD -i $INTERFACE -o eth1 -m mac --mac-source $MAC_DEVICE -j DROP; iptables -D  FORWARD -i eth1 -o $INTERFACE -j DROP -d $IP_DEVICE' SIGINT
+
+
+filter="(ether src  $MAC_DEVICE and dst host 10.11.0.1)"
+
+for i in 1
+do
+   echo "Experiment $i"
+   EXP_FOLDER="Result/$MAC_DEVICE/Experiments/Experiment_$i"
+   mkdir -p "$EXP_FOLDER"
+   
+   sleep 2s
+    tshark -i "$INTERFACE" -f "$filter" -w "$EXP_FOLDER/capture.pcap" -a duration:"$tshark_time" &
+    sleep 20s
+
+python3 ../Test/statusNanoLeaf.py "off"
+    sleep 5s
+   python3 ../Test/statusNanoLeaf.py "on"
+sleep 5s
+python3 ../Test/statusNanoLeaf.py "off"
+    sleep 5s
+   python3 ../Test/statusNanoLeaf.py "on"
+sleep 5s
+python3 ../Test/statusNanoLeaf.py "off"
+    sleep 5s
+   python3 ../Test/statusNanoLeaf.py "on"
+sleep 5s
+python3 ../Test/statusNanoLeaf.py "off"
+    sleep 5s
+   python3 ../Test/statusNanoLeaf.py "on"
+sleep 5s
+python3 ../Test/statusNanoLeaf.py "off"
+    sleep 5s
+   python3 ../Test/statusNanoLeaf.py "on"
+sleep 5s
+
+   wait
+   chmod +r "$EXP_FOLDER"/capture.pcap
+   python3 TrainingReplay.py "$EXP_FOLDER/capture.pcap" "$EXP_FOLDER" $MAC_SMARTPHONE $MAC_DEVICE  > $EXP_FOLDER/accuracy.txt
+done
+
+chmod -R 777 Result
+
+
+#iptables -D FORWARD -i $INTERFACE -o eth1 -m mac --mac-source $MAC_DEVICE -j DROP
+#iptables -D  FORWARD -i eth1 -o $INTERFACE -j DROP -d $IP_DEVICE
